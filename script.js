504fec6cd2cdd9b01ade9b1cdf3e3de21aee1f3e
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-19 17:24:44 +0200

    declared empty functions

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
+let li = document.createElement("li");
 
 function inputLength() {
     return input.value.length;
 }
 
+function addListElement() {
+
+}
+
+function createListContent() {
+
+}
+
+function getInputValue () {
+
+}
