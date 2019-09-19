2078946c8221c33629749b4eb6d8ad7321968ec5
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-19 17:15:08 +0200

    added inputLength function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
+
+function inputLength() {
+    return input.value.length;
+}
+
