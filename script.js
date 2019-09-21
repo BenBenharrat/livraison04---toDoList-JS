2ca6a22a33531ed8720607822d2e100f0900bfea
BenBenharrat <benjaminbenharrat@gmail.com>
2019-09-21 15:11:22 +0200

    declared li variable

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
-
+let li;
 
 function getInputLength() {
     return input.value.length;
